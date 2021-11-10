234a25e715dad52b56f5dbaa8d5941f7ac9abac4
Pedro Santos <90897030+psantos19@users.noreply.github.com>
2021-11-10 09:33:08 +0000

    Add files via upload

---
+++ spotopark/src/main/java/pt/iade/spotopark/SpotoparkApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.spotopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotoparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotoparkApplication.class, args);
+	}
+
+}
---
+++ spotopark/src/test/java/pt/iade/spotopark/SpotoparkApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.spotopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotoparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
