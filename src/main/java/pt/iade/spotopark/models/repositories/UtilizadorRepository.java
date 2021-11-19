03ac6a3173380ee439f583ebbba3e8f72fd07183
Pedro Santos <pedrojgmsantos@gmail.com>
2021-11-19 00:50:49 +0000

    commit

--- src/main/java/pt/iade/spotopark/models/repositories/UtilizadorRepository.java
+++ src/main/java/pt/iade/spotopark/models/repositories/UtilizadorRepository.java
@@ -1,6 +1,5 @@
 package pt.iade.spotopark.models.repositories;
 
-
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,12 +9,12 @@ import org.springframework.data.repository.query.Param;
 
 import pt.iade.spotopark.models.utilizador;
 
-public interface UtilizadorRepository extends CrudRepository<utilizador , Integer>{
-    @Modifying @Transactional
-    @Query(value="insert into utilizador (user_id, user_name, user_password, user_bdate," +
-        "user_email, user_coordinates)" +
-        "values(:#{#user_id}, :#{#user_name}, :#{#user_password}, "+
-        ":#{#user_bdate}, :#{#user_email}"+
-        ", :#{#user_coordinates})", nativeQuery=true)
+public interface UtilizadorRepository extends CrudRepository<utilizador, Integer> {
+    @Modifying
+    @Transactional
+    @Query(value = "insert into utilizador (user_id, user_name, user_password, user_bdate,"
+            + "user_email, user_coordinates)"
+            + "values(:#{#utilizador.id}, :#{#utilizador.name}, :#{#utilizador.password}, "
+            + ":#{#utilizador.bdate}, :#{#utilizador.email}" + ", :#{#utilizador.coordinates})", nativeQuery = true)
     Integer registerUtilizador(@Param("utilizador") utilizador utilizador);
 }
