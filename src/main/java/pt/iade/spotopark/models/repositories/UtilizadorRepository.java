92d928e41d133bb05b5c577f0f4e73bba8b8035a
Pedro Santos <pedrojgmsantos@gmail.com>
2021-11-23 08:50:35 +0000

    commit

--- src/main/java/pt/iade/spotopark/models/repositories/UtilizadorRepository.java
+++ src/main/java/pt/iade/spotopark/models/repositories/UtilizadorRepository.java
@@ -12,9 +12,8 @@ import pt.iade.spotopark.models.utilizador;
 public interface UtilizadorRepository extends CrudRepository<utilizador, Integer> {
     @Modifying
     @Transactional
-    @Query(value = "insert into utilizador (user_name, user_password, user_bdate, " 
-            + "user_email, user_coordinates)"
-            + " values(:#{#utilizador.name}, :#{#utilizador.password}, "
+    @Query(value = "insert into utilizador (utilizador_name, utilizador_password, utilizador_bdate, "
+            + "utilizador_email, utilizador_coordinates)" + " values(:#{#utilizador.name}, :#{#utilizador.password}, "
             + ":#{#utilizador.bdate}, :#{#utilizador.email}" + ", :#{#utilizador.coordinates})", nativeQuery = true)
     Integer registerUtilizador(@Param("utilizador") utilizador utilizador);
 }
